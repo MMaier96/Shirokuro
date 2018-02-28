e8b72b0f79b49e4643a36dfa8470e8707c81608c
marcel <marcelmaier.htc@gmail.com>
2018-02-28 12:32:53 +0100

    update

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,7 +6,7 @@ public class Main  {
 
 	
 	public static void main(String... args) {
-		
+		//TODO test
 		DotList.createDotList();
 		Application app = new Application();
 		app.setArguments(args);
